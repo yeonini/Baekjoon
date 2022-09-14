2a2ad4b123beafeef0cf4a8368523ccfd827ec83
yeonini <102447989+yeonini@users.noreply.github.com>
2022-09-14 21:50:16 +0900

    Delete src directory

--- src/S1_13_10171.java
+++
@@ -1,8 +0,0 @@
-public class S1_13_10171 {
-    public static void main(String[] args) {
-        System.out.println("\\    /\\");
-        System.out.println(" )  ( ')");
-        System.out.println("(  /  )");
-        System.out.println(" \\(__)|");
-    }
-}
