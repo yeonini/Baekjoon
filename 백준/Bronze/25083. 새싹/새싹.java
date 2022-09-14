23f130ca2d8841261fdc9a2d951f475cabe95394
yeonini <102447989+yeonini@users.noreply.github.com>
2022-09-14 21:55:36 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13896 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+            System.out.println("         ,r'\"7");  // 큰 따옴표 앞에 역슬래쉬
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");    // 역슬래쉬 앞에 역슬래쉬
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
