70fe4b6c5dbe05bd9a57d96db88431ae7ffd7469
yeonini <102447989+yeonini@users.noreply.github.com>
2022-10-08 22:12:38 +0900

    [Silver V] Title: 영화감독 숌, Time: 304 ms, Memory: 86092 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -7,21 +7,19 @@ public class Main {
         // N 받아오기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        
-        int count = 0; // N번째 수 확인용 count
-        int number = 666; // 1번째 수인 666부터 하나씩 더할 예정
-        String str = ""; // number를 문자열로 변환해서 contains쓸 예정
-        
+
+        int count = 1; // N번째 수 확인용 count
+        int number = 666; // 1번째 수인 666부터 하나씩 더할 예정\
+
         // count가 N보다 작을 때 반복
         while(count < N){
-            str = String.valueOf(number);
-            // 문자열에 666들어 있으면 count 더하기
-            if (str.contains("666")) {
-                count++;
-            }
             // 숫자 하나씩 더하기
             number++;
+            // 문자열에 666들어 있으면 count 더하기
+            if ((String.valueOf(number)).contains("666")) {
+                count++;
+            }
         }
-        System.out.println(Integer.parseInt(str));
+        System.out.println(number);
     }
 }
