956ad9dc4cf96bb0684092558cf3b76393970551
yeonini <102447989+yeonini@users.noreply.github.com>
2022-10-27 22:17:28 +0900

    [Bronze II] Title: 블랙잭, Time: 128 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -7,28 +7,32 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // N, M
+        /* N(오픈된 카드 수), M(딜러 숫자) 값 넣기 */
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        // 카드 숫자 담을 배열
+        /* 배열에 오픈된 카드들의 숫자 담기 */
         int[] arr = new int[N];
-
-        // 배열에 카드 숫자 담기
         st = new StringTokenizer(br.readLine(), " ");
+        
         for (int i = 0; i < N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        /* M을 넘지 않는 최대 합계 maxSum 구하기 (조합 이용) */
         int maxSum = 0;
-        // 반복문으로 조합구해서 합계 구하기
-        // 합계가 maxSum보다 크고, M 이하이면 maxSum에 넣어주기
+        
         for(int i = 0; i < arr.length; i++){
+            
             for(int j = i+1; j < arr.length; j++){
+                
                 for(int k = j+1; k < arr.length; k++){
                     int sum = arr[i] + arr[j] + arr[k];
-                    if (maxSum < sum && sum <= M) {maxSum = sum;}
+                    
+                    if (maxSum < sum && sum <= M) {
+                        maxSum = sum;
+                    }
                 }
             }
         }
