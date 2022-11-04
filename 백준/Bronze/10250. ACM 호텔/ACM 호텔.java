2702cb31a4cd8af74514f4390dd5d24c0e0ab91c
yeonini <102447989+yeonini@users.noreply.github.com>
2022-11-04 21:26:31 +0900

    [Bronze II] Title: ACM 호텔, Time: 132 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -1,35 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // T 받아오기
+        /* T(테스트케이스 수) 받아오기 */
         int T = Integer.parseInt(br.readLine());
 
-        // T만큼 만큼
         while(T > 0){
-            // H, W, N 받아오기
+            T--;
+            /* 토크나이저로 H(전체 층수), W(전체 호수), N(손님) 받아오기 */
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int H = Integer.parseInt(st.nextToken());
             int W = Integer.parseInt(st.nextToken());
             int N = Integer.parseInt(st.nextToken());
-
-            int h = 0; //층 수
-            int w = 0; //호 수
-
+            
+            /* 손님에게 배정될 층수(h), 호수(w) */
+            int h = 0;
+            int w = 0;
+            
+            /* 방 번호 구하기 */
+            /* 꼭대기 층(전체 층(H)이 손님 수(N)의 약수) */
             if (N % H == 0) {
                 h = H * 100;
                 w = N / H;
             }
+           
             else {
                 h = (N % H) * 100;
                 w = N / H + 1;
             }
-            System.out.println(h + w); //결과 출력
-            T--;
+            System.out.println(h + w);
         }
     }
 }
+/* ex. 3층(H), 4호(W)짜리 건물에 3번째(N) 손님, 4번째(N) 손님 
+□ □ □ □     □ □ □ □     3 □ □ □     ■ □ □ □
+□ □ □ □     2 □ □ □     ■ □ □ □     ■ □ □ □
+1 □ □ □     ■ □ □ □     ■ □ □ □     ■ 4 □ □
+101호       201호      301호(3/3=1) 102호(4/3+1=2)
+ */
