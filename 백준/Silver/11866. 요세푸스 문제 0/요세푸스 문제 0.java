37cfbfb60d4fe05565894e1ab70888e3d5f38faa
yeonini <102447989+yeonini@users.noreply.github.com>
2022-11-26 15:33:10 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 128 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,36 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //N, K 받아오기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken()); // N명
+        int K = Integer.parseInt(st.nextToken()); // K번째
+        
+        br.close();
 
-        // 1부터 N까지 가지는 리스트 생성
+        /* 1 ~ N 리스트 생성 */
         ArrayList<Integer> list = new ArrayList<>(N);
         for (int i = 1; i <= N; i++){
             list.add(i);
         }
 
         StringBuilder sb = new StringBuilder("<");
-        // K씩 증가하는 인덱스 값을 삭제
+
+        /* 인덱스 K씩 증가하면서 삭제 */
+        /* idx += K-1 에서 K-1인 이유
+        1. list가 0이 아닌 1부터 N까지여서
+        2. 앞선 순서에서 하나 삭제했기 때문에 인덱스 번호 맞추려고 1개 빼고 더한 것
+         */
         int idx = 0;
         while (list.size() != 0){
-            idx += K-1; // list가 0부터 시작하는게 아니여서 인덱스가 밀리기때문에 -1
-            if (idx > list.size()-1){ // idx가 사이즈 넘으면 나눈 나머지로..
+            idx += K-1;
+
+            /* 리스트 사이즈 넘으면 나머지값으로 바꿔주기 */
+            if (idx > list.size()-1){
                 idx %= list.size();
             }
-            sb.append(list.get(idx)).append(", "); //sb에 삭제하는 문자 넣기
-            list.remove(idx); // 리스트에서 삭제
+
+            /* 빌더에 넣어주고 리스트에서 숫자 삭제 */
+            sb.append(list.get(idx)).append(", ");
+            list.remove(idx);
         }
 
         sb.setLength(sb.length() - 2); //마지막 ", " 지우기
+
         sb.append(">");
         System.out.println(sb);
     }
