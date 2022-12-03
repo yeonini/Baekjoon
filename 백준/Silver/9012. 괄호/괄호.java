3f28ece4796cea5b8b53aae21968e7877b12ebc8
yeonini <102447989+yeonini@users.noreply.github.com>
2022-12-03 15:43:05 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,42 +1,43 @@
 //9012 괄호
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        /* 숫자 N 받아서 N번 반복문 만들기 */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder("");
         int N = Integer.parseInt(br.readLine());
 
-        StringBuilder sb = new StringBuilder("");
-
         for (int i = 0; i < N; i++) {
-            /* 스택 만들고, '('이면 문자 넣고, ')'이면 문자 빼는 방식으로 진행 예정 */
             Stack<Character> st = new Stack<>();
+
+            String result = "YES";
 
             /* 문자열 받아서 문자 순회 */
             String str = br.readLine();
-            String result = "YES";
             for (int j = 0; j < str.length(); j++) {
-                /* '('이면 스택에 1 넣고, ')'이면 빼기, 빼기전에 스택 비어있으면 NO */
+                /* if : '('이면 스택에 넣기 
+                 * else if : '(' 먼저 들어가있지 않으면 바로 NO로 끝내주기
+                 * else : '(' 아닌 경우 ')'니까 pop 으로 빼주기*/
                 if (str.charAt(j) == '(') {
                     st.push('(');
-                } else if (st.empty()) {
+                }else if (st.empty()) {
                     result = "NO";
                     break;
-                } else {
+                }else {
                     st.pop();
                 }
             }
-            
-            /* 마지막에 스택 비어있지 않으면(=남은 1 있으면) NO */
+
+            /* 마지막에 스택 비어있지 않으면(=남은 문자 있으면) NO */
             if (!st.empty()) {
                 result = "NO";
             }
+
             sb.append(result).append("\n");
         }
+        br.close();
+
         System.out.println(sb);
     }
 }
